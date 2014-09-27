c01fd811eaac43d422552c58f09b228f7a8ad9b7
Brad Ballard <brad@bradballard.com>
2014-09-27 11:56:40 -0400

    Site commit

---
+++ js/cufon-replace.js
@@ -0,0 +1,5 @@
+Cufon.replace('.sf-menu > li > a, .button, .button-1, .button-2, .button-3, .button-4',{fontFamily:'Maven Pro Bold', hover:true})
+Cufon.replace('h1, h4, h5, h2, .box-title, .price, .cap',{fontFamily:'Maven Pro Bold'})
+Cufon.replace('.dropcap',{fontFamily:'Maven Pro Bold', textShadow:"1px 1px 0 #90aaca"})
+Cufon.replace('.text-1',{fontFamily:'Maven Pro Bold'})
+Cufon.replace('.text-2, .text-4',{fontFamily:'Maven Pro Bold', textShadow:"1px 1px 0 #979a9d"})
